7f48045f07ba6f1c768327661f38d00637901135
Stefan Boesen <stefan.boesen@gmail.com>
2012-01-03 16:34:10 -0800

    Derp. Needs to be wrapped in document.ready...

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
-$('input').keyup(function(){
-  console.log('keyup');
-  if ($(this).text() == ''){
-    console.log('msg blank');
-    $(this).text('Type something above!'); 
-  }
-  else {
-    console.log('lol');
-    $('.changeme').text('');
-    var parsedDate = $(this).datetimeparse();
-    $('.changeme').text(parsedDate.toString());
-  }
+console.log("In test.js");
+
+$(document).ready(function() {
+  $('input').keyup(function(){
+    console.log("keyup");
+    if ($(this).text() == ''){
+      console.log('msg blank');
+      $(this).text('Type something above!'); 
+    }
+    else {
+      console.log('lol');
+      $('.changeme').text('');
+      var parsedDate = $(this).datetimeparse();
+      $('.changeme').text(parsedDate.toString());
+    }
+  });
 });
