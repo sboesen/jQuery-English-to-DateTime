9321a20c0a5e2862250c548d616ee14ec1df4106
Stefan Boesen <stefan.boesen@gmail.com>
2012-01-03 17:59:31 -0800

    Removed console logs in test as it is working 100% (apart from not displaying the value of the date returned… hmmmm)

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
-console.log("In test.js");
-
 $(document).ready(function() {
   $('input').keyup(function(){
-    console.log("keyup");
-    if ($(this).text() == ''){
-      console.log('msg blank');
-      $(this).text('Type something above!'); 
+    var inputText = $(this).val();
+    if (inputText == ''){
+      $('.changeme').text('Type something above!'); 
     }
     else {
-      console.log('lol');
       $('.changeme').text('');
       var parsedDate = $(this).datetimeparse();
-      $('.changeme').text(parsedDate.toString());
+      $('.changeme').text(parsedDate);
     }
   });
 });
